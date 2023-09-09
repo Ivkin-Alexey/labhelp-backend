89730e56acc71b15d6762024bc8e0e69e9ebfcf3
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-09-09 13:32:32 +0300

    debug

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ const webAppUrl = 'https://ephemeral-kringle-2c94b2.netlify.app/';
 const {doc} = googleSpreadsheetAPIServices;
 const app = express();
 
-const token = "5925873875:AAHc7TDnpGFo1aYo4x_52SmN7ATn97_BmPA";
-
 const PORT = 443;
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
 
 app.use(express.json());
 app.use(cors());
