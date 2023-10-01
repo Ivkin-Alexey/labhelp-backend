16721db42cea5dc9d07bb14dc6642b007e52fef7
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-10-02 00:11:07 +0300

    debug

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ app.post('/web-data', async (req, res) => {
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
-                message_text: `Следующие данные отправлены: ${formData}`,
+                message_text: `Следующие данные отправлены: ${JSON.stringify(formData)}`,
             }
         })
         return res.status(200).json({queryId});
