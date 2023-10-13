cb349c9753b0ba860cb2b584823e2fe4b0eba06e
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-10-13 19:14:31 +0300

    debug

--- index.js
+++ index.js
@@ -160,17 +160,17 @@ bot.on('callback_query', async ctx => {
 
 app.post('/web-data', async (req, res) => {
     const {queryId, formData} = req.body;
-    console.log(req, res, formData);
+    // console.log(req, res, formData);
     try {
-        await updateUserData(chatID, formData);
-        // await bot.answerWebAppQuery(queryId, {
-        //     type: 'article',
-        //     id: queryId,
-        //     title: 'Успешная покупка',
-        //     input_message_content: {
-        //         message_text: `Следующие данные отправлены: ${JSON.stringify(formData)}`,
-        //     }
-        // })
+        // await updateUserData(chatID, formData);
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: `Следующие данные отправлены: ${JSON.stringify(req, res, formData)}`,
+            }
+        })
         return res.status(200).json({queryId});
     } catch (e) {
         return res.status(500).json({});
