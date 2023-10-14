c3d53a97841c94665dde4e930e98ee6dbc1f11be
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-10-14 21:32:53 +0300

    debug

--- methods/botAnswers.js
+++ methods/botAnswers.js
@@ -29,7 +29,8 @@ async function sendResearches(bot, chatID) {
 }
 
 async function sendResearch(bot, chatID, researchTopic) {
-    const editProfileUrl = webAppUrl + `/${chatID}/editProfile`;
+    const editProfileUrl = webAppUrl + `${chatID}/editProfile`;
+    console.log(editProfileUrl);
     const research = researches.find(el => el.name === researchTopic);
     const {id, degree, advisor} = research;
     const imageStream = fs.createReadStream(`./assets/images/${id}.jpg`);
