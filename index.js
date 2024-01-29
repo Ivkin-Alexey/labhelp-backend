0b2cc093ee0bf414bed00bb76a0dd70f75c9890d
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-01-29 23:45:32 +0300

    debug

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ const http = require("http");
 const https = require("https");
 const fs = require("fs");
 const {PORT, HTTPS_PORT} = require("./assets/constants/constants");
+const {getEquipmentList} = require("./methods/equipments");
+const {getUserList} = require("./methods/users");
+const {researchesSelectOptions} = require("./assets/constants/researches");
+const {
+    updateUserDataPost,
+    deletePersonPost,
+    equipmentStartPost,
+    equipmentEndPost
+} = require("./methods/appPostsProcessing");
 
 const app = express();
 app.use(express.json());
@@ -37,5 +46,37 @@ httpServer.listen(PORT, () => {
 httpsServer.listen(HTTPS_PORT, () => {
     console.log(`HTTPS Server running on port ${HTTPS_PORT}`);
 })
+
+app.get('/hello', async (req, res) => {
+    return res.status(200).json('Привет');
+});
+app.get('/equipmentList', async (req, res) => {
+    try {
+        return await getEquipmentList().then(equipmentList => res.status(200).json(equipmentList))
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+});
+
+app.get('/persons', async (req, res) => {
+    try {
+        return await getUserList().then(personList => res.status(200).json(personList))
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+});
+
+app.get('/researches', async (req, res) => {
+    try {
+        return res.status(200).json(researchesSelectOptions);
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+});
+
+app.post("/updatePersonData", async (req, res) => await updateUserDataPost(req, res, bot));
+app.post("/deletePerson", async (req, res) => await deletePersonPost(req, res, bot));
+app.post("/equipmentStart", async (req, res) => await equipmentStartPost(req, res, bot))
+app.post("/equipmentEnd", async (req, res) => await equipmentEndPost(req, res, bot));
 
 module.exports = {app};
--- server.js
+++ server.js
@@ -13,31 +13,3 @@ app.get('/hello', async (req, res) => {
     return res.status(200).json('Привет');
 });
 
-app.get('/equipmentList', async (req, res) => {
-    try {
-        return await getEquipmentList().then(equipmentList => res.status(200).json(equipmentList))
-    } catch (e) {
-        return res.status(500).json(e);
-    }
-});
-
-app.get('/persons', async (req, res) => {
-    try {
-        return await getUserList().then(personList => res.status(200).json(personList))
-    } catch (e) {
-        return res.status(500).json(e);
-    }
-});
-
-app.get('/researches', async (req, res) => {
-    try {
-        return res.status(200).json(researchesSelectOptions);
-    } catch (e) {
-        return res.status(500).json(e);
-    }
-});
-
-app.post("/updatePersonData", async (req, res) => await updateUserDataPost(req, res, bot));
-app.post("/deletePerson", async (req, res) => await deletePersonPost(req, res, bot));
-app.post("/equipmentStart", async (req, res) => await equipmentStartPost(req, res, bot))
-app.post("/equipmentEnd", async (req, res) => await equipmentEndPost(req, res, bot));
