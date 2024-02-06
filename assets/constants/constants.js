58e97398ab9aa371f78eefc40f06b26ddfe75ae1
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-02-06 23:36:52 +0300

    debug

--- assets/constants/constants.js
+++ assets/constants/constants.js
@@ -6,7 +6,7 @@ const HTTPS_PORT = 443;
 
 const timeZoneRelativeToUTC = 3;
 const webAppUrl = 'https://ephemeral-kringle-2c94b2.netlify.app';
-const editProfileUrl = webAppUrl + '/392584400/editProfile';
+const editProfileUrl = webAppUrl + '/1516784245/editProfile';
 const smiles = {
     researches: "👨‍ ‍🔬 ",
     closeMenu: "❌ ",
