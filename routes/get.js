c49ab4d5b663391cb618a3896e2863f9045c3656
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-06-17 04:46:07 +0300

    feat: add route for person data getting

--- routes/get.js
+++ routes/get.js
@@ -1,5 +1,5 @@
 import {getEquipmentList} from "../methods/equipments.js";
-import {getUserList} from "../methods/users.js";
+import {getUserData, getUserList} from "../methods/users.js";
 import{researchesSelectOptions} from "../assets/constants/researches.js";
 import {getReagentApplications, addNewReagentAppToDB} from "../methods/reagents.js";
 import {getWorkingEquipmentListFromDB} from "../methods/db/equipment.js";
@@ -24,8 +24,17 @@ export default function get(app) {
             return res.status(500).json(e);
         }
     });
+
+    app.get('/person/:chatID', async (req, res) => {
+        try {
+            const chatID = req.params.chatID;
+            return await getUserData(chatID).then(person => res.status(200).json(person));
+        } catch (e) {
+            return res.status(500).json(e);
+        }
+    });
     
-    app.get('/persons', async (req, res) => {
+    app.get('/persons/:chatID', async (req, res) => {
         try {
             return await getUserList().then(personList => res.status(200).json(personList));
         } catch (e) {
