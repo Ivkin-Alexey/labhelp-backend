3de95bfa2651d0fbbdeff95b94b8ea9f22589c42
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-07-26 10:15:35 +0300

    debug

--- routes/get.js
+++ routes/get.js
@@ -24,7 +24,6 @@ export default function get(app) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
 
