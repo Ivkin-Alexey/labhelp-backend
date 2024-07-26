5377b9ffceb24d821205483a1da70b1f8cddd01e
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-07-26 09:34:23 +0300

    debug

--- routes/get.js
+++ routes/get.js
@@ -18,15 +18,16 @@ import {
 } from "../methods/db/equipment.js";
 import { generateAccessToken, authenticateToken } from "../methods/jwt.js";
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 export default function get(app) {
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
   app.get("/hello", async (req, res) => {
     return res.status(200).json("Привет");
   });
