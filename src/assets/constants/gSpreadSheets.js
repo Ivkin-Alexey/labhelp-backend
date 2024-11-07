62ca222d5a8a5563268ef17b9ea659593c0b358f
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-11-07 21:24:14 +0300

    feat: change gsheet list ID with equipment data

--- src/assets/constants/gSpreadSheets.js
+++ src/assets/constants/gSpreadSheets.js
@@ -7,7 +7,7 @@ const equipmentOperationsTableID = '1UZ21neg8OevoKReVYHIAkUmw9v388zF8AwpykXs-EsE
 const equipmentOperationsSheetIndex = '0'
 const confirmedUsersTableID = '19R0KhK7RvVPKvtmVA7-EGPjakJxfWRflRLHp4Z4FFrs'
 const equipmentListTableID = '1DzK7-8XCBOmPmmtTpVOR_kEYh1oVfyQD4sUODwKmQK0'
-const equipmentListSheetID = '0'
+const equipmentListSheetID = '460529131'
 
 const serviceAccountAuth = new JWT.JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
