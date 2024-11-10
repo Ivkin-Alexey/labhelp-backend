71c7e696946d593a8b2145e1fd72e7af0235e691
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-11-11 00:32:34 +0300

    feat: implement error processing for JWT token

--- src/controllers/jwt.js
+++ src/controllers/jwt.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import { jwtTokenSecret } from '../../index.js'
 import { jwtLifeTime } from '../assets/constants/constants.js'
+import { sendError } from './tg-bot-controllers/botAnswers.js'
 
 function generateAccessToken(login, role = 'user') {
   return jwt.sign(
@@ -19,10 +20,20 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
-  if (token == null) return res.sendStatus(401)
+  if (token == null) {
+    const msg = `Отсутствует JWT-токен. Запрос по адресу: ${req.url}`
+    console.error(msg)
+    sendError(msg)
+    return res.status(401).json({message: msg, status: 401})
+  }
 
   jwt.verify(token, jwtTokenSecret, (err, user) => {
-    if (err) return res.sendStatus(403)
+    if (err) {
+      const msg = `Некорректный JWT-токен. Запрос по адресу: ${req.url}`
+      console.error(msg)
+      sendError(msg)
+      return res.status(403).json({message: msg, status: 403})
+    }
     req.user = user
     next()
   })
