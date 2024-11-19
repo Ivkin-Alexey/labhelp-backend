a2275268b6f89f1863b27928794416e47e81a20e
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-11-20 00:00:55 +0300

    debug

--- src/routes/delete.js
+++ src/routes/delete.js
@@ -1,9 +1,12 @@
+import { processFavoriteEquipmentResponse } from '../controllers/equipment-controller/favorite-equipment.js'
 import { changeOperateEquipmentStatus } from '../controllers/equipment-controller/operate-equipment.js'
 import { processSearchHistoryRequest } from '../controllers/equipment-controller/search-history.js'
 import { processUserRequest } from '../controllers/user-controller.js'
 
 export default function deleteMethod(app) {
   app.delete('/equipments/operate/:equipmentId', changeOperateEquipmentStatus)
+
+  app.delete('/equipments/favorite/:equipmentId', processFavoriteEquipmentResponse)
 
   app.delete('/users/:login', processUserRequest)
 
